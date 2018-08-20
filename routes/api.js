ab41ab26ff7d4401ec9e68b3bdab4861ab605cc0
Glitch (opposite-sundial) <none>
2018-08-20 12:42:18 +0000

    🍮✌️ Checkpoint ./routes/api.js:531805/646 ./package.json:531805/1253

--- routes/api.js
+++ routes/api.js
@@ -10,6 +10,7 @@
 
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
+var request = require('request');
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
@@ -17,9 +18,16 @@ module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      let stock = req.params;
+      let stock = req.query.stock;
       let url = "https://api.iextrading.com/1.0/stock/"+stock+"/quote";
-      console.log(stock);
+      request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          let data = JSON.parse(body);
+          res.json({
+            
+          }) // Show the HTML for the Google homepage. 
+        }
+      });
     });
     
 };
