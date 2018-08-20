ca8c3b81bd4971d76aa0969d8f1e9b84572dfc23
Glitch (opposite-sundial) <none>
2018-08-20 12:52:24 +0000

    📹🎂 Checkpoint ./routes/api.js:531805/857 ./controllers/stockHandler.js:531805/1171

---
+++ controllers/stockHandler.js
@@ -0,0 +1,16 @@
+var request = require('request');
+
+let handleStock = function(stock){
+  let url = "https://api.iextrading.com/1.0/stock/"+stock+"/quote";
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      let data = JSON.parse(body);
+      let stockData = {
+        stock: data.symbol,
+        price: data.open
+      }
+
+      return stockData
+    }
+  });
+}
--- routes/api.js
+++ routes/api.js
@@ -10,7 +10,6 @@
 
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
-var request = require('request');
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
@@ -18,16 +17,8 @@ module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      let stock = req.query.stock;
-      let url = "https://api.iextrading.com/1.0/stock/"+stock+"/quote";
-      request(url, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          let data = JSON.parse(body);
-          res.json({
-            
-          }) // Show the HTML for the Google homepage. 
-        }
-      });
+      console.log(req.query);
+      
     });
     
 };
