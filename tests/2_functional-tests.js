2109084c8a69e976b424432863f42466e2e2340b
Glitch (opposite-sundial) <none>
2018-08-20 12:11:21 +0000

    🎹🎉 Checkpoint ./tests/2_functional-tests.js:531805/1266

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -22,23 +22,48 @@ suite('Functional Tests', function() {
         .get('/api/stock-prices')
         .query({stock: 'goog'})
         .end(function(err, res){
-          
-          //complete this one too
-          
+          assert.equal(res.status, 200);
+          assert.equal(res.body.stockData.stock, 'GOOG');
           done();
         });
       });
       
       test('1 stock with like', function(done) {
-        
+        chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: 'goog', like: true})
+        .end(function(err,res){
+          assert.equal(res.status, 200);
+          assert.equal(res.body.stockData.stock, 'GOOG');
+          assert.equal(res.body.stockData.likes, 1);
+          done()
+        })
       });
       
       test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        
+        chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: 'goog', like: true})
+        .end(function(err,res){
+          assert.equal(res.status, 200);
+          assert.equal(res.body.stockData.stock, 'GOOG');
+          assert.equal(res.body.stockData.likes, 1);
+          done();
+        })
       });
       
       test('2 stocks', function(done) {
-        
+        chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: 'goog', stock: 'msft'})
+        .end(function(err,res){
+          assert.equal(res.status, 200);
+          assert.equal(res.body.stockData[0].stock, 'GOOG');
+          assert.equal(res.body.stockData[1].stock, 'MSFT');
+          assert.equal(res.body.stockData[0].rel_likes, 0);
+          
+          done();
+        })
       });
       
       test('2 stocks with like', function(done) {
