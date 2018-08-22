407af825baf5cff6f522efa0668899333e6b721a
Glitch (opposite-sundial) <none>
2018-08-22 01:34:31 +0000

    🏣🛬 Checkpoint ./routes/api.js:531805/429

--- routes/api.js
+++ routes/api.js
@@ -13,7 +13,7 @@ var request = require('request');
 var fetch = require('node-fetch');
 
 async function fetchStock(stock){
-  let url = "https://api.iextrading.com/1.0/stock/"+stock+"/quote";
+  let url = 
   let response = await fetch(url);
   let data = await response.json();
   //console.log(data);
@@ -27,38 +27,47 @@ module.exports = function (app,db) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
+      let url   = "https://api.iextrading.com/1.0/stock/"+stock+"/quote";
       let stock = req.query.stock;
       let like  = req.query.like || false;
+      let ip    = req.ip
     
       // one stock without like --- see any previous like exixts in database
+      
+      if(!Array.isArray(stock) && !like){
+        fetch(url)
+        .then((data => data.json()
+      }
+      
       // one stock with like --- save like api to database 
       // two stocks without like --- save like and show rel_likes
       // two stocks with like --- no database access needed, rel_likes always 0;
     
-      if(!Array.isArray(stock) && !like){
-        oneStockWithoutLike(stock);
-      } else if(!Array.isArray(stock) && like){
-        oneStockWithLike(stock)
-      }
+//       if(!Array.isArray(stock) && !like){
+//         oneStockWithoutLike(stock);
+//       } else if(!Array.isArray(stock) && like){
+//         oneStockWithLike(stock)
+//       }
     
-      function likeExists(stock){
-        db.collection(req.ip).findOne({name: stock})
-        .then((data) => {
-          if(data){
-            return data;
-          }
-        })
-        .catch(err => console.log(err))
-      }
-    
-      function oneStockWithoutLike(stock){
-        console.log(likeExists(stock));
+//       function likeExists(stock){
         
-      }
+//         db.collection(ip).findOne({name: stock})
+//         .then((data) => {
+          
+//             console.log(data);
+          
+//         })
+//         .catch(err => console.log(err))
+//       }
+    
+//       function oneStockWithoutLike(stock){
+//         likeExists(stock);
+        
+//       }
       
-      function oneStockWithLike(stock){
+//       function oneStockWithLike(stock){
         
-      }
+//       }
       
     
       // Array.isArray() to check if two stocks
