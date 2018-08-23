cf32fb8717fccdfaa984f6f5984d745388c3a11d
Glitch (opposite-sundial) <none>
2018-08-23 02:51:30 +0000

    🐑🍉 Checkpoint ./routes/api.js:531805/3607

--- routes/api.js
+++ routes/api.js
@@ -12,105 +12,51 @@ var expect    = require('chai').expect;
 var request = require('request');
 var fetch = require('node-fetch');
 
-async function fetchStock(stock, like){
+// grab symbol and price from third party api
+async function fetchStock(stock){
   let url = "https://api.iextrading.com/1.0/stock/"+stock+"/quote";
   let response = await fetch(url);
   let data = await response.json();
   //console.log(data);
   if(data){
-    let stockData = {
+    return {
       stock: data.symbol,
-      price: data.open,
-      likes: like 
+      price: data.open
     }
-    
-    return stockData;
   }
 };
+
+
   
 
 module.exports = function (app,db) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      //let url   = "https://api.iextrading.com/1.0/stock/"+stock+"/quote";
       let stock = req.query.stock;
       let like  = req.query.like || false;
       let ip    = req.ip
       
-      // one stock without like --- see any previous like exixts in database
-      
+      async function findLike(stock){
+        let response = await db.collection(ip).findOne({name:stock});
+        return response;
+      }
+    
+      async function saveLike(stock){
+        let response = await db.collection(ip).insertOne({name: stock});
+        return response;
+      }
+    
+      function relLikes(obj1, obj2){
+        return obj1.likes - obj2.likes;
+      }
+    
+      // if one stock and no likes 
+    
       if(!Array.isArray(stock) && !like){
-         db.collection(ip).findOne({name: stock})
-         .then((data) => {
-           if(!data){
-             fetchStock(stock, 0).then((data) => res.json(data));
-           } 
-           
-      if (!Array.isArray(stock) && like{
-             fetchStock(stock, 1).then((data) => res.json(data));
-           }
-         }).catch(err => console.log(err))
+        findLike
       }
-        
-      if(!Array.isArray(stock) && like){
-        db.collection(ip).findOne({name: stock})
-        .then((data) => {
-          if(!data){
-            db.collection(ip).insertOne({name: stock}, (data, err) => {
-              if(data.insertedCount===1){
-                fetchStock(stock, 1).then((data) => res.json(data));
-              } else {
-                console.log(err);
-              }
-            })
-          } else {
-            fetchStock(stock, 1).then((data) => res.json(data));
-          }
-        })
-      }
-      // one stock with like --- save like api to database 
-      // two stocks without like --- save like and show rel_likes
-      // two stocks with like --- no database access needed, rel_likes always 0;
-    
-//       if(!Array.isArray(stock) && !like){
-//         oneStockWithoutLike(stock);
-//       } else if(!Array.isArray(stock) && like){
-//         oneStockWithLike(stock)
-//       }
-    
-//       function likeExists(stock){
-        
-//         db.collection(ip).findOne({name: stock})
-//         .then((data) => {
-          
-//             console.log(data);
-          
-//         })
-//         .catch(err => console.log(err))
-//       }
-    
-//       function oneStockWithoutLike(stock){
-//         likeExists(stock);
-        
-//       }
       
-//       function oneStockWithLike(stock){
-        
-//       }
-      
-    
-      // Array.isArray() to check if two stocks
-      // like false if no like
-      // if no like do not need to save ip
-      
-      // collection as ip name, save liked stocks
-    
-      // if(!like && !Array.isArray(stock)){
-      //   let stockData = data('goog');
-      //   //stockData.likes = 0;
-      //   res.json({stockData});
-      // }
     });
     
 };
