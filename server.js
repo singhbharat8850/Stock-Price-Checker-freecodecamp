f86d247287cc3431dc36476745c99c0216949fd7
Tanim Mahmud <tanimmahmud@users.noreply.github.com>
2018-08-26 21:53:26 +1000

    add content security policy

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
+var helmet      = require('helmet')
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
@@ -15,6 +16,15 @@ var MongoClient = require('mongodb');
 const CONNECTION_STRING = process.env.DB; 
 
 var app = express();
+
+app.use(helmet());
+
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    styleSrc: ["'self'"]
+  }
+}))
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
